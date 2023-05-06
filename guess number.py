f08e8d8e547f38c8abcd01dd83d331626d332019
Manu V <132737827+1010manu@users.noreply.github.com>
2023-05-06 12:31:53 +0530

    Add files via upload

---
+++ guess number.py
@@ -0,0 +1,12 @@
+import random
+number=random.randint(1,100)
+guess=0
+while guess!=number:
+    guess=int(input('enter:'))
+    if guess<number:
+        print('guess higher')
+    elif guess>number:
+        print('guess lower')
+    else:
+        print('guessed it')
+        
